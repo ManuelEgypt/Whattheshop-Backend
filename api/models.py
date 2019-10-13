8ad109a0d9d21c130e27b92d197fedd8c20a4122
samirahisham <engineersamiraawad@gmail.com>
2019-10-13 13:33:40 +0300

    added package model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class Package(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    image = models.ImageField()
+    price = models.TextField()
+    duration = models.CharField(max_length=10)
+    destination = models.TextField()
+    # tours = models.TextField()
+    
